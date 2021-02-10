5b1f9af8aff58ad5bf6290cd9400eb15a762a38a
須貝克仁 <>
2021-02-11 02:55:22 +0900

    no message

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -64,6 +64,6 @@ class ItemsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def item_params
-      params.require(:item).permit(:name, :description)
+      params.require(:item).permit(:name, :description, images: [])
     end
 end
